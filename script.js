c80c94293c33618ef424e17bee635f11b60663a9
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-05-17 20:55:24 +0800

    Operate Function
    Operate function carries out the arithmetic operations for the two operands.

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
 function add(a,b){
     return a+b;
 }
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function operate(a,b,operator){
+    switch (operator){
+        case '+':
+            return add(a,b);
+
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '÷':
+            return divide(a,b);
+    };
+}
+
+function updateOperand(){
+    const operand = this.textContent;
+    const currentOperand = document.querySelector('[data-current-operand');
+    currentOperand.textContent = `${currentOperand.textContent}${operand}`;
+    console.log(currentOperand);
+}
+
+function updateOperator(){
+    const operator = this.textContent;
+    const previousOperand = document.querySelector('[data-previous-operand');
+    const currentOperand = document.querySelector('[data-current-operand');
+    const currentOperator = document.querySelector('[data-input-operator]');
+    let output;
+    if (currentOperator.textContent !== ''){
+        previousOperand.textContent = operate(+previousOperand.textContent, +currentOperand.textContent, currentOperator.textContent);
+       
+    }
+    else{
+        previousOperand.textContent = currentOperand.textContent;
+        
+    }
+    currentOperator.textContent = `${operator}`;
+    currentOperand.textContent = '';
+    
+    console.log(operator);
+}
+
+function clearInput(){
+    const currentOperand = document.querySelector('[data-current-operand');
+    const previousOperand = document.querySelector('[data-previous-operand');
+    const operator = document.querySelector('[data-input-operator');
+    previousOperand.textContent = '';
+    currentOperand.textContent = '';
+    operator.textContent = '';
+}
+
+const numbers = document.querySelectorAll('[data-number]');
+numbers.forEach(number => number.addEventListener('click', updateOperand))
+
+const operators = document.querySelectorAll('[data-operator]');
+operators.forEach(operator => operator.addEventListener('click', updateOperator))
+
+const clear = document.querySelector('[data-clear]');
+clear.addEventListener('click', clearInput);
